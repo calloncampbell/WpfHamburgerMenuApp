e83b9fb90f2fafe55ce1d5aa0827498d8c5991d9
Callon Campbell <calloncampbell@msn.com>
2018-06-20 12:23:09 -0400

    Add project files.

---
+++ WpfHamburgerMenuApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHamburgerMenuApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //TODO
+            //UserControl usc = null;
+            //GridMain.Children.Clear();
+
+            //switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            //{
+            //    case "ItemHome":
+            //        usc = new UserControlHome();
+            //        GridMain.Children.Add(usc);
+            //        break;
+            //    case "ItemCreate":
+            //        usc = new UserControlCreate();
+            //        GridMain.Children.Add(usc);
+            //        break;
+            //    default:
+            //        break;
+            //}
+        }
+
+        private void ButtonOpenSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonOpenHelp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
